94b91b3e44dd855209f19065087cc9330b1ecfad
Sam@Unique <aymantheremkus11@gmail.com>
2025-11-12 22:16:46 +0300

    Add Project Model And User Relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,4 +50,8 @@ class User extends Authenticatable
     public function tasks(): HasMany {
         return $this -> hasMany(Task::class);
     }
+
+    public function projects(): HasMany {
+        return $this -> hasMany(Project::class);
+    }
 }
