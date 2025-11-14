ee2094ac81cae37fcb7d55d08b79ab06a68f4a90
Sam@Unique <aymantheremkus11@gmail.com>
2025-11-14 11:50:51 +0300

    Add Friendly Dates Display

--- resources/views/tasks/project/show.blade.php
+++ resources/views/tasks/project/show.blade.php
@@ -7,7 +7,7 @@
         <p>{{ $projectTask -> task }}</p>
         <hr>
         <p>Completed: {{ $projectTask -> completed ? "True" : "False" }}</p>
-        <p>Created At: {{ $projectTask -> created_at }}</p>
+        <p>Created At: {{ $projectTask -> created_at -> diffForHumans() }}</p>
         <a href="{{ route('projects.projectTasks.edit', [$project, $projectTask]) }}">Edit</a>
 
         <form method="POST" action="{{ route('projects.projectTasks.destroy', [$project, $projectTask]) }}">
